c98e246821b6086a39146563b439c6b6e5b5995f
animesh <royanimesh2211@gmail.com>
2018-04-20 00:48:31 +0600

    Added UserDetails.java and activity_user_details.xml

--- app/src/main/java/com/developervishalsehgal/udacityscholarsapp/UserDetails.java
+++ app/src/main/java/com/developervishalsehgal/udacityscholarsapp/UserDetails.java
@@ -1,13 +1,67 @@
 package com.developervishalsehgal.udacityscholarsapp;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.developervishalsehgal.udacityscholarsapp.IntroActivity.IntroActivity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserDetails extends AppCompatActivity {
+
+    Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_details);
+        button = (Button) findViewById(R.id.buttonUserDetails);
+
+        EditText mEdit = (EditText) findViewById(R.id.editTextEmail);
+        mEdit.setEnabled(false);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(UserDetails.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+//get the spinner from the xml.
+        Spinner dropdown = findViewById(R.id.spinner1);
+//create a list of items for the spinner.
+        String[] items = new String[]{"Web Developer Beginner", "Web Developer Intermediate", "Android Developer Beginner","Android Developer Intermediate"};
+//create an adapter to describe how the items are displayed, adapters are used in several places in android.
+//There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+//set the spinners adapter to the previously created one.
+        dropdown.setAdapter(adapter);
+
     }
+
+
+    public void onBackPressed(){
+        Intent a = new Intent(Intent.ACTION_MAIN);
+        a.addCategory(Intent.CATEGORY_HOME);
+        a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(a);
+
+    }
+
 }

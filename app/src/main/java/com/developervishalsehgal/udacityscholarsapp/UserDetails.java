3aadc65277899995394a02530dd47ec9a79eaf0c
animesh <royanimesh2211@gmail.com>
2018-04-20 00:39:06 +0600

    Added Google Account like Icon

---
+++ app/src/main/java/com/developervishalsehgal/udacityscholarsapp/UserDetails.java
@@ -0,0 +1,13 @@
+package com.developervishalsehgal.udacityscholarsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_details);
+    }
+}

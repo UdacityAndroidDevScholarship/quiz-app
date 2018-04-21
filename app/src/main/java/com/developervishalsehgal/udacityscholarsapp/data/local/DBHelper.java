a83bba0b12f353363104c5ff728c9958148a7977
nihanth876 <34400175+nihanth876@users.noreply.github.com>
2018-04-21 19:58:35 +0530

    Update DBHelper.java

--- app/src/main/java/com/developervishalsehgal/udacityscholarsapp/data/local/DBHelper.java
+++ app/src/main/java/com/developervishalsehgal/udacityscholarsapp/data/local/DBHelper.java
@@ -17,17 +17,36 @@ public class DBHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "quiz_app.db";
     private static final int DATABASE_VERSION = 1;
 
+    private static final String CREATE_NOTIFICATION_TABLE =
+            "CREATE TABLE " + NotificationContract.NotificationEntry.TABLE_NAME + " (" +
+                    NotificationContract.NotificationEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"+
+                    NotificationContract.NotificationEntry.COLUMN_TIMESTAMP + " INTEGER, " +
+                    NotificationContract.NotificationEntry.COLUMN_TITLE + " TEXT NOT NULL, " +
+                    NotificationContract.NotificationEntry.COLUMN_DESCRIPTION + " TEXT NOT NULL, " +
+                    NotificationContract.NotificationEntry.COLUMN_FROM + " TEXT NOT NULL, " +
+                    NotificationContract.NotificationEntry.COLUMN_TYPE + " TEXT NOT NULL, " +
+                    NotificationContract.NotificationEntry.COLUMN_ACTION + " TEXT , " +
+                    NotificationContract.NotificationEntry.COLUMN_EXTRA1 + " TEXT, " +
+                    NotificationContract.NotificationEntry.COLUMN_EXTRA2 + " TEXT" +
+                    ");";
+
+    private static final String DELETE_NOTIFICATION_TABLE =
+            "DROP TABLE IF EXISTS " + NotificationContract.NotificationEntry.TABLE_NAME;
+
+
     public DBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL(CREATE_NOTIFICATION_TABLE);     //creates a table
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        //db.execSQL(DELETE_NOTIFICATION_TABLE);
+        //can be added if wanted
     }
 }
+

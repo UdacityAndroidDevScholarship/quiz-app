350894144c657fc376fa72160e0108288b1c547c
pramodbharti <pramodbharti.info@gmail.com>
2018-04-23 13:40:06 +0530

    Made changes according to schema

--- app/src/main/java/com/developervishalsehgal/udacityscholarsapp/data/local/DBHelper.java
+++ app/src/main/java/com/developervishalsehgal/udacityscholarsapp/data/local/DBHelper.java
@@ -4,15 +4,18 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
+
 import com.developervishalsehgal.udacityscholarsapp.data.local.QuestionsContract.QuestionsEntry;
 
 /**
  * Local database for the app. Contains only one database which all the related tables.
  * Notifications, Attempt Quiz, Create Quiz, Forums, Cache etc.
- *
+ * <p>
  * TODO change the description after implementation
  *
  * @Author kaushald
+ * @Author pramodbharti | Have implemented OnCreate() method for TABLE questions and options
+ *
  */
 public class DBHelper extends SQLiteOpenHelper {
 
@@ -27,22 +30,24 @@ public class DBHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
 
-        String SQL_QUESTIONS_CREATE = "CREATE TABLE " + QuestionsEntry.TBL_QUESTIONS + " ("
-                + QuestionsEntry.QUE_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-                + QuestionsEntry.QUE_DESC + " TEXT NOT NULL, "
-                + QuestionsEntry.QUE_IMG + " TEXT, "
-                + QuestionsEntry.QUE_TYPE + " INTEGER NOT NULL, "
-                + QuestionsEntry.MARKS + " INTEGER NOT NULL DEFAULT 0);";
-        Log.e(LOG_TAG,SQL_QUESTIONS_CREATE);
+        String SQL_QUESTIONS_CREATE = "CREATE TABLE " + QuestionsEntry.TABLE_QUESTIONS + " ("
+                + QuestionsEntry.COLUMN_QUESTION_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + QuestionsEntry.COLUMN_QUESTION_DESC + " TEXT NOT NULL, "
+                + QuestionsEntry.COLUMN_QUESTION_TYPE + " TEXT NOT NULL, "
+                + QuestionsEntry.COLUMN_QUESTION_MARKS + " INTEGER NOT NULL);";
+        Log.e(LOG_TAG, SQL_QUESTIONS_CREATE);
         db.execSQL(SQL_QUESTIONS_CREATE);
 
-        String SQL_OPTIONS_CREATE = "CREATE TABLE " + QuestionsEntry.TBL_OPTIONS + " ("
-                + QuestionsEntry.OPT_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-                + QuestionsEntry.FK_QUE_ID + " INTEGER NOT NULL, "
-                + QuestionsEntry.OPT_DESC + " TEXT NOT NULL, "
-                + QuestionsEntry.IS_CORRECT + " BOOLEAN NOT NULL, "
-                + QuestionsEntry.REMARKS + " TEXT);";
-        Log.e(LOG_TAG,SQL_OPTIONS_CREATE);
+        String SQL_OPTIONS_CREATE = "CREATE TABLE " + QuestionsEntry.TABLE_OPTIONS + " ("
+                + QuestionsEntry.COLUMN_OPTION_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + QuestionsEntry.COLUMN_OPTION_QUESTION_ID + " INTEGER NOT NULL, "
+                + QuestionsEntry.COLUMN_OPTION_DESC + " TEXT NOT NULL, "
+                + QuestionsEntry.COLUMN_OPTION_IS_CORRECT + " BOOLEAN NOT NULL, "
+                + QuestionsEntry.COLUMN_OPTION_REMARKS + " TEXT, FOREIGN KEY("
+                + QuestionsEntry.COLUMN_OPTION_QUESTION_ID + ") REFERENCES "
+                + QuestionsEntry.TABLE_QUESTIONS + "("
+                + QuestionsEntry.COLUMN_QUESTION_ID + "));";
+        Log.e(LOG_TAG, SQL_OPTIONS_CREATE);
         db.execSQL(SQL_OPTIONS_CREATE);
 
     }
--- app/src/main/java/com/developervishalsehgal/udacityscholarsapp/data/local/QuestionsContract.java
+++ app/src/main/java/com/developervishalsehgal/udacityscholarsapp/data/local/QuestionsContract.java
@@ -9,37 +9,22 @@ import android.provider.BaseColumns;
 public class QuestionsContract {
     public static abstract class QuestionsEntry implements BaseColumns {
         //Table name for questions.
-        public static final String TBL_QUESTIONS = "tbl_questions";
-        //All column name for table TBL_QUESTIONS.
-        public static final String QUE_ID = BaseColumns._ID;
-        public static final String QUE_DESC = "que_desc";
-        public static final String QUE_IMG = "bg_image";
-        public static final String QUE_TYPE = "que_type";
-        public static final String MARKS = "marks";
-
-        //Constant for que_type column
-        public static final int SUBJECTIVE = 0;
-        public static final int SINGLE_CHOICE = 1;
-        public static final int MULTIPLE_CHOICE = 2;
+        public static final String TABLE_QUESTIONS = "questions";
+        //All column name for table TABLE_QUESTIONS.
+        public static final String COLUMN_QUESTION_ID = "id";
+        public static final String COLUMN_QUESTION_DESC = "description";
+        public static final String COLUMN_QUESTION_TYPE = "type";
+        public static final String COLUMN_QUESTION_MARKS = "marks";
 
         //Table name for options.
-        public static final String TBL_OPTIONS = "tbl_options";
-        //All column name for table TBL_OPTIONS.
-        public static final String OPT_ID = BaseColumns._ID;
-        //Foreign key referencing TBL_QUESTIONS QUE_ID
-        public static final String FK_QUE_ID = "que_id";
-        public static final String OPT_DESC = "opt_desc";
-        public static final String IS_CORRECT = "is_correct";
-        public static final String REMARKS = "remarks";
+        public static final String TABLE_OPTIONS = "options";
+        //All column name for table TABLE_OPTIONS.
+        public static final String COLUMN_OPTION_ID = "id";
+        //Foreign key referencing TABLE_QUESTIONS's COLUMN_QUESTION_ID
+        public static final String COLUMN_OPTION_QUESTION_ID = "question_id";
+        public static final String COLUMN_OPTION_DESC = "description";
+        public static final String COLUMN_OPTION_IS_CORRECT = "is_correct";
+        public static final String COLUMN_OPTION_REMARKS = "remarks";
 
-        /**
-         * Returns whether or not the given question type is {@link #SUBJECTIVE}, {@link #SINGLE_CHOICE,
-         * or {@link #MULTIPLE_CHOICE}.
-         */
-        public static boolean isValidQueType(int type) {
-            if (type == SUBJECTIVE || type == SINGLE_CHOICE || type == MULTIPLE_CHOICE)
-                return true;
-            return false;
-        }
     }
 }

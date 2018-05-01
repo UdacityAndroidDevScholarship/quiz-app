89bcc9766a828a19a60eaf6f114c353d6b256ec9
Intkhab Ahmed <intkhab.ahmed64@gmail.com>
2018-05-01 14:09:56 +0530

    Created a private method to make notification object and raise notification

--- app/src/main/java/com/developervishalsehgal/udacityscholarsapp/services/FBMessagingService.java
+++ app/src/main/java/com/developervishalsehgal/udacityscholarsapp/services/FBMessagingService.java
@@ -1,6 +1,5 @@
 package com.developervishalsehgal.udacityscholarsapp.services;
 
-import com.developervishalsehgal.udacityscholarsapp.data.local.NotificationContract;
 import com.developervishalsehgal.udacityscholarsapp.data.models.Notification;
 import com.developervishalsehgal.udacityscholarsapp.utils.NotificationUtils;
 import com.google.firebase.messaging.FirebaseMessagingService;
@@ -10,22 +9,37 @@ import java.util.Map;
 
 public class FBMessagingService extends FirebaseMessagingService{
 
+    private static final String KEY_TITLE = "title";
+    private static final String KEY_ACTION = "action";
+    private static final String KEY_DESCRIPTION = "description";
+    private static final String KEY_FROM = "from";
+    private static final String KEY_TYPE = "type";
+    private static final String KEY_EXTRA_1 = "extra_1";
+    private static final String KEY_EXTRA_2 = "extra_2";
+
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
-        Map<String, String> receivedData = remoteMessage.getData();
-        Notification notification = new Notification();
-        notification.setTitle(receivedData.get(NotificationContract.NotificationEntry.COLUMN_TITLE));
-        notification.setAction(receivedData.get(NotificationContract.NotificationEntry.COLUMN_ACTION));
-        notification.setDescription(receivedData.get(NotificationContract.NotificationEntry.COLUMN_DESCRIPTION));
-        notification.setFrom(receivedData.get(NotificationContract.NotificationEntry.COLUMN_FROM));
-        notification.setType(receivedData.get(NotificationContract.NotificationEntry.COLUMN_TYPE));
-        notification.setExtra1(receivedData.get(NotificationContract.NotificationEntry.COLUMN_EXTRA_1));
-        notification.setExtra2(receivedData.get(NotificationContract.NotificationEntry.COLUMN_EXTRA_2));
-
-        NotificationUtils.createNotification(this, notification, notification.getType());
-        // TODO: Check for data and notification payload from remoteMessage and handle it accordingly
+        if(remoteMessage != null) {
+            raiseSystemNotification(remoteMessage);
+        }
     }
 
+    /**
+     * Description - raises the system notification when the payload is received from server
+     * @param remoteMessage - Payload received from server
+     */
+    private void raiseSystemNotification(RemoteMessage remoteMessage) {
+        Map<String, String> receivedData = remoteMessage.getData();
+        Notification notification = new Notification();
+        notification.setTitle(receivedData.get(KEY_TITLE));
+        notification.setAction(receivedData.get(KEY_ACTION));
+        notification.setDescription(receivedData.get(KEY_DESCRIPTION));
+        notification.setFrom(receivedData.get(KEY_FROM));
+        notification.setType(receivedData.get(KEY_TYPE));
+        notification.setExtra1(receivedData.get(KEY_EXTRA_1));
+        notification.setExtra2(receivedData.get(KEY_EXTRA_2));
 
+        NotificationUtils.createNotification(this, notification, notification.getType());
+    }
 }

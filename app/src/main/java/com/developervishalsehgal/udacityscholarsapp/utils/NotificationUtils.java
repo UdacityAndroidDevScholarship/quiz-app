bd7d37a6902447db5547b1d2f09776c47daf041a
Intkhab Ahmed <intkhab.ahmed64@gmail.com>
2018-05-01 14:11:52 +0530

    Created a private method to create a unique notification id and added action,extras to the pending intent and some helper strings

--- app/src/main/java/com/developervishalsehgal/udacityscholarsapp/utils/NotificationUtils.java
+++ app/src/main/java/com/developervishalsehgal/udacityscholarsapp/utils/NotificationUtils.java
@@ -6,16 +6,21 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Build;
+import android.preference.PreferenceManager;
 import android.support.annotation.DrawableRes;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.ContextCompat;
 
 import com.developervishalsehgal.udacityscholarsapp.R;
+import com.developervishalsehgal.udacityscholarsapp.data.local.NotificationContract;
 import com.developervishalsehgal.udacityscholarsapp.data.models.Notification;
 import com.developervishalsehgal.udacityscholarsapp.ui.home.HomeActivity;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class NotificationUtils {
 
@@ -42,7 +47,6 @@ public class NotificationUtils {
      * @param channelId - Id for the notification channel
      */
     public static void createNotification(Context context, Notification notification, String channelId) {
-        int NOTIFICATION_ID = 100;
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         if(Build.VERSION.SDK_INT >= 26) {
             createNotificationChannel(context, channelId, notification.getType());
@@ -56,7 +60,8 @@ public class NotificationUtils {
                 .setStyle(new NotificationCompat.BigTextStyle().bigText(
                         notification.getDescription()))
                 .setDefaults(android.app.Notification.DEFAULT_VIBRATE)
-                .setContentIntent(createContentIntent(context))
+                .setContentIntent(createContentIntent(context, notification.getAction(),
+                        notification.getExtra1(), notification.getExtra2()))
                 /*.addAction(drinkWaterAction(context))
                 .addAction(ignoreReminderAction(context))*/
                 .setAutoCancel(true);
@@ -65,7 +70,7 @@ public class NotificationUtils {
             builder.setPriority(NotificationCompat.PRIORITY_HIGH);
         }
         if (notificationManager != null) {
-            notificationManager.notify(NOTIFICATION_ID, builder.build());
+            notificationManager.notify(createUniqueNotificationId(context), builder.build());
         }
     }
 
@@ -74,8 +79,11 @@ public class NotificationUtils {
      * @param context - Context of the calling activity
      * @return - returns PendingIntent
      */
-    private static PendingIntent createContentIntent(Context context) {
+    private static PendingIntent createContentIntent(Context context, String action, String extra1, String extra2) {
         Intent intent = new Intent(context, HomeActivity.class);
+        intent.setAction(action);
+        intent.putExtra(NotificationContract.NotificationEntry.COLUMN_EXTRA_1, extra1);
+        intent.putExtra(NotificationContract.NotificationEntry.COLUMN_EXTRA_2, extra2);
         return PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
     }
 
@@ -125,5 +133,27 @@ public class NotificationUtils {
                 default:
                     return 0;
         }
+    }
+
+    /**
+     * Description - creates a unique notification id. Retrieves the previously created id from sharedpreferences
+     * and increment by one to ensure uniqueness. We are using sharedpreferences to ensure that previous created id
+     * is not created again
+     * @param context - Context of the calling activity
+     * @return notificationId - returns a unique notification id
+     */
+    private static int createUniqueNotificationId(Context context) {
+        //Get the previous id from the sharedpreferences to ensure it is unique all time even when app is closed and reopened
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        int notificationId = sharedPreferences.getInt(context.getString(R.string.notification_id_key),
+                Integer.parseInt(context.getString(R.string.default_notification_id)));
+        //Initialize with the previous id and increment by one to get new id
+        AtomicInteger uniqueId = new AtomicInteger(notificationId);
+        notificationId = uniqueId.incrementAndGet();
+        //Update the latest notificationId in the sharedpreferences
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt(context.getString(R.string.notification_id_key), notificationId);
+        editor.apply();
+        return notificationId;
     }
 }

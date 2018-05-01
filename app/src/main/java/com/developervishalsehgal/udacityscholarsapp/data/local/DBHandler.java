3132ec88bc5b9efe4ef5e45a62f8d70c71e43ee4
rahul1993 <rahulsinghal0308@gmail.com>
2018-05-01 18:38:03 +0530

    Moved the method querying cursor for column index out of do-while loop. And used NotificationContract.NotificationEntry as reference for the notification column.

--- app/src/main/java/com/developervishalsehgal/udacityscholarsapp/data/local/DBHandler.java
+++ app/src/main/java/com/developervishalsehgal/udacityscholarsapp/data/local/DBHandler.java
@@ -10,7 +10,7 @@ import com.developervishalsehgal.udacityscholarsapp.data.models.Notification;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.developervishalsehgal.udacityscholarsapp.data.local.NotificationContract.*;
+import com.developervishalsehgal.udacityscholarsapp.data.local.NotificationContract;
 /**
  * The oly point of interaction with local Database
  *
@@ -40,29 +40,28 @@ public class DBHandler {
 //    Add notification details to the local database
     public void addNotification(Notification notification){
         ContentValues contentValues = new ContentValues();
-        contentValues.put(NotificationEntry.COLUMN_TIMESTAMP, notification.getTimeStamp());
-        contentValues.put(NotificationEntry.COLUMN_TITLE, notification.getTitle());
-        contentValues.put(NotificationEntry.COLUMN_DESCRIPTION, notification.getDescription());
-        contentValues.put(NotificationEntry.COLUMN_FROM, notification.getFrom());
-        contentValues.put(NotificationEntry.COLUMN_TYPE, notification.getType());
-        contentValues.put(NotificationEntry.COLUMN_ACTION, notification.getAction());
-        contentValues.put(NotificationEntry.COLUMN_EXTRA_1, notification.getExtra1());
-        contentValues.put(NotificationEntry.COLUMN_EXTRA_2, notification.getExtra2());
+        contentValues.put(NotificationContract.NotificationEntry.COLUMN_TIMESTAMP, notification.getTimeStamp());
+        contentValues.put(NotificationContract.NotificationEntry.COLUMN_TITLE, notification.getTitle());
+        contentValues.put(NotificationContract.NotificationEntry.COLUMN_DESCRIPTION, notification.getDescription());
+        contentValues.put(NotificationContract.NotificationEntry.COLUMN_FROM, notification.getFrom());
+        contentValues.put(NotificationContract.NotificationEntry.COLUMN_TYPE, notification.getType());
+        contentValues.put(NotificationContract.NotificationEntry.COLUMN_ACTION, notification.getAction());
+        contentValues.put(NotificationContract.NotificationEntry.COLUMN_EXTRA_1, notification.getExtra1());
+        contentValues.put(NotificationContract.NotificationEntry.COLUMN_EXTRA_2, notification.getExtra2());
 
         db.insert(NotificationContract.NotificationEntry.TABLE_NAME, null, contentValues);
     }
 
 //    List of column selected while querying the database
     private static final String[] PROJECTION_ALL_COLUMN = new String[] {
-        NotificationEntry.COLUMN_TIMESTAMP,                       //0
-        NotificationEntry.COLUMN_TITLE,                           //1
-        NotificationEntry.COLUMN_DESCRIPTION,                     //2
-        NotificationEntry.COLUMN_FROM,                            //3
-        NotificationEntry.COLUMN_TYPE,                            //4
-        NotificationEntry.COLUMN_ACTION,                          //5
-        NotificationEntry.COLUMN_EXTRA_1,                         //6
-        NotificationEntry.COLUMN_EXTRA_2};                        //7
-
+    NotificationContract.NotificationEntry.COLUMN_TIMESTAMP,                       //0
+    NotificationContract.NotificationEntry.COLUMN_TITLE,                           //1
+    NotificationContract.NotificationEntry.COLUMN_DESCRIPTION,                     //2
+    NotificationContract.NotificationEntry.COLUMN_FROM,                            //3
+    NotificationContract.NotificationEntry.COLUMN_TYPE,                            //4
+    NotificationContract.NotificationEntry.COLUMN_ACTION,                          //5
+    NotificationContract.NotificationEntry.COLUMN_EXTRA_1,                         //6
+    NotificationContract.NotificationEntry.COLUMN_EXTRA_2};                        //7
 
 //    Query the database to get all notification availabe
     public List<Notification> getAllNotification(int startFrom, int limit){
@@ -70,18 +69,26 @@ public class DBHandler {
         Notification notification = new Notification();
 
         Cursor cursor =  db.query(NotificationContract.NotificationEntry.TABLE_NAME, PROJECTION_ALL_COLUMN, null,
-            null, null, null, NotificationEntry.COLUMN_TIMESTAMP +" DESC");
+            null, null, null, NotificationContract.NotificationEntry.COLUMN_TIMESTAMP +" DESC");
         try {
+            int timeStampIndex = cursor.getColumnIndex(NotificationContract.NotificationEntry.COLUMN_TIMESTAMP);
+            int titleIndex = cursor.getColumnIndex(NotificationContract.NotificationEntry.COLUMN_TITLE);
+            int descriptionIndex = cursor.getColumnIndex(NotificationContract.NotificationEntry.COLUMN_DESCRIPTION);
+            int fromIndex = cursor.getColumnIndex(NotificationContract.NotificationEntry.COLUMN_FROM);
+            int typeIndex = cursor.getColumnIndex(NotificationContract.NotificationEntry.COLUMN_TYPE);
+            int actionIndex = cursor.getColumnIndex(NotificationContract.NotificationEntry.COLUMN_ACTION);
+            int extra1Index = cursor.getColumnIndex(NotificationContract.NotificationEntry.COLUMN_EXTRA_2);
+            int extra2Index = cursor.getColumnIndex(NotificationContract.NotificationEntry.COLUMN_EXTRA_1);
             cursor.moveToFirst();
             do {
-                notification.setTimeStamp(cursor.getLong(cursor.getColumnIndex(NotificationEntry.COLUMN_TIMESTAMP)));
-                notification.setTitle(cursor.getString(cursor.getColumnIndex(NotificationEntry.COLUMN_TITLE)));
-                notification.setDescription(cursor.getString(cursor.getColumnIndex(NotificationEntry.COLUMN_DESCRIPTION)));
-                notification.setFrom(cursor.getString(cursor.getColumnIndex(NotificationEntry.COLUMN_FROM)));
-                notification.setType(cursor.getString(cursor.getColumnIndex(NotificationEntry.COLUMN_TYPE)));
-                notification.setAction(cursor.getString(cursor.getColumnIndex(NotificationEntry.COLUMN_ACTION)));
-                notification.setExtra1(cursor.getString(cursor.getColumnIndex(NotificationEntry.COLUMN_EXTRA_1)));
-                notification.setExtra2(cursor.getString(cursor.getColumnIndex(NotificationEntry.COLUMN_EXTRA_2)));
+                notification.setTimeStamp(cursor.getLong(timeStampIndex));
+                notification.setTitle(cursor.getString(titleIndex));
+                notification.setDescription(cursor.getString(descriptionIndex));
+                notification.setFrom(cursor.getString(fromIndex));
+                notification.setType(cursor.getString(typeIndex));
+                notification.setAction(cursor.getString(actionIndex));
+                notification.setExtra1(cursor.getString(extra1Index));
+                notification.setExtra2(cursor.getString(extra2Index));
                 notificationList.add(notification);
 
             } while (cursor.moveToNext());
@@ -105,19 +112,27 @@ public class DBHandler {
         String[] selectionCriteria = {"%"+query+"%", "%"+query+"%"};
 
         Cursor cursor = db.query(NotificationContract.NotificationEntry.TABLE_NAME, PROJECTION_ALL_COLUMN, selectionStatement, selectionCriteria,
-            null,null, NotificationEntry.COLUMN_TIMESTAMP + " DESC");
+            null,null, NotificationContract.NotificationEntry.COLUMN_TIMESTAMP + " DESC");
 
         try {
+            int timeStampIndex = cursor.getColumnIndex(NotificationContract.NotificationEntry.COLUMN_TIMESTAMP);
+            int titleIndex = cursor.getColumnIndex(NotificationContract.NotificationEntry.COLUMN_TITLE);
+            int descriptionIndex = cursor.getColumnIndex(NotificationContract.NotificationEntry.COLUMN_DESCRIPTION);
+            int fromIndex = cursor.getColumnIndex(NotificationContract.NotificationEntry.COLUMN_FROM);
+            int typeIndex = cursor.getColumnIndex(NotificationContract.NotificationEntry.COLUMN_TYPE);
+            int actionIndex = cursor.getColumnIndex(NotificationContract.NotificationEntry.COLUMN_ACTION);
+            int extra1Index = cursor.getColumnIndex(NotificationContract.NotificationEntry.COLUMN_EXTRA_1);
+            int extra2Index = cursor.getColumnIndex(NotificationContract.NotificationEntry.COLUMN_EXTRA_2);
             cursor.moveToFirst();
                 do{
-                    searchNotification.setTimeStamp(cursor.getLong(cursor.getColumnIndex(NotificationEntry.COLUMN_TIMESTAMP)));
-                    searchNotification.setTitle(cursor.getString(cursor.getColumnIndex(NotificationEntry.COLUMN_TITLE)));
-                    searchNotification.setDescription(cursor.getString(cursor.getColumnIndex(NotificationEntry.COLUMN_DESCRIPTION)));
-                    searchNotification.setFrom(cursor.getString(cursor.getColumnIndex(NotificationEntry.COLUMN_FROM)));
-                    searchNotification.setType(cursor.getString(cursor.getColumnIndex(NotificationEntry.COLUMN_TYPE)));
-                    searchNotification.setAction(cursor.getString(cursor.getColumnIndex(NotificationEntry.COLUMN_ACTION)));
-                    searchNotification.setExtra1(cursor.getString(cursor.getColumnIndex(NotificationEntry.COLUMN_EXTRA_1)));
-                    searchNotification.setExtra2(cursor.getString(cursor.getColumnIndex(NotificationEntry.COLUMN_EXTRA_2)));
+                    searchNotification.setTimeStamp(cursor.getLong(timeStampIndex));
+                    searchNotification.setTitle(cursor.getString(titleIndex));
+                    searchNotification.setDescription(cursor.getString(descriptionIndex));
+                    searchNotification.setFrom(cursor.getString(fromIndex));
+                    searchNotification.setType(cursor.getString(typeIndex));
+                    searchNotification.setAction(cursor.getString(actionIndex));
+                    searchNotification.setExtra1(cursor.getString(extra1Index));
+                    searchNotification.setExtra2(cursor.getString(extra2Index));
                     searchNotificationList.add(searchNotification);
                 }while (cursor.moveToNext());
         } catch (Exception e){

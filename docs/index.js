592b42af2670e57a3720f7e5d565c031c41fea94
Kaushal <kaushal.devil009@gmail.com>
2018-05-12 19:08:36 +0530

    Updated firebase cloud function index.js file in docs

--- docs/index.js
+++ docs/index.js
@@ -57,4 +57,56 @@ exports.sendNewResourceNotification = functions.database.ref('/resources/{resour
       return console.log('gcms sent successfully: ' + response);
     });
   });
-})
+});
+
+exports.sendNewCommentNotification = functions.database.ref('/discussions/{quizId}/{commentId}').onCreate((snapshot, context) => {
+
+    const comment = snapshot.val();
+  
+    // if the data is empty exit from the function.
+    if(!comment){
+      return console.log('No data for discussion comment');
+    }
+  
+    // Only send notification for new resource not any update in the database
+    //   if(resource.previous.val()){
+    //     return;
+    //   }
+  
+    const commentId = context.params.commentId;
+    const quizId = context.params.quizId;
+
+    console.log('New Comment added for. '+quizId+'->' + comment.comment);
+  
+    var payload = {
+      data: {
+        title: 'New Comment by ' + comment["comment-by"],
+        description: comment.comment,
+        sender: comment["comment-by"],
+        type: 'discussion',
+        action: quizId,
+        extra1: comment["commenter-id"],
+        extra2: commentId
+      }
+    };
+  
+    return admin.database().ref('/users').once('value').then(allUsers => {
+      //console.log('all users: ' + allUsers);
+      var tokens = new Array();
+      allUsers.forEach(function(snapshot){
+        var key = snapshot.key;
+        var singleToken = snapshot.val()['fcm-token'];
+        if(singleToken){
+          tokens.push(singleToken);
+        }
+      });
+      // var fcmTokens = allUsers.val().filter(user => {
+      //     return user.email === 'drulabs@gmail.com';
+      // }).map(user => {
+      //     return user['fcm-token'];
+      // });
+      return admin.messaging().sendToDevice(tokens, payload).then(response => {
+        return console.log('gcms sent successfully: ' + response);
+      });
+    });
+  })

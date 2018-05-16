a964d5cb72a6c0bfcd863ef38ebf15754237a59a
animesh <royanimesh2211@gmail.com>
2018-05-16 22:55:24 +0600

    Alert dialog added for no internet

--- app/src/main/java/com/developervishalsehgal/udacityscholarsapp/ui/home/HomeActivity.java
+++ app/src/main/java/com/developervishalsehgal/udacityscholarsapp/ui/home/HomeActivity.java
@@ -3,7 +3,12 @@ package com.developervishalsehgal.udacityscholarsapp.ui.home;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.ValueAnimator;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
@@ -100,6 +105,8 @@ public class HomeActivity extends AppCompatActivity implements HomeContract.View
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if(!isConnected(HomeActivity.this)) buildDialog(HomeActivity.this).show();
+
         // Injecting presenter reference
         PresenterInjector.injectHomePresenter(this);
 
@@ -114,6 +121,45 @@ public class HomeActivity extends AppCompatActivity implements HomeContract.View
         displaySplashScreen();
 
         setUpSwipeRefresh();
+    }
+
+    public boolean isConnected(Context context) {
+
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netinfo = cm.getActiveNetworkInfo();
+
+        if (netinfo != null && netinfo.isConnectedOrConnecting()) {
+            android.net.NetworkInfo wifi = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+            android.net.NetworkInfo mobile = cm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+
+            if((mobile != null && mobile.isConnectedOrConnecting()) || (wifi != null && wifi.isConnectedOrConnecting())) return true;
+            else return false;
+        } else
+            return false;
+    }
+
+    public AlertDialog.Builder buildDialog(Context c) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(c);
+        builder.setTitle("Your Data Is Off");
+        builder.setMessage("Turn on data or wifi in settings");
+
+        builder.setPositiveButton("Exit", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                finish();
+            }
+        });
+        builder.setNegativeButton("settings", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                startActivityForResult(new Intent(android.provider.Settings.ACTION_SETTINGS), 0);
+            }
+        });
+
+        return builder;
     }
 
     private void initializeUI() {
